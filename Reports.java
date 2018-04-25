377eea30b3d9cb2a04d74a6bd5be55900dfcb643
cpryan98 <38046584+cpryan98@users.noreply.github.com>
2018-04-25 15:25:48 -0400

    Update Reports.java

--- Reports.java
+++ Reports.java
@@ -45,5 +45,42 @@ public class Reports {
         stmt1.close();
         rset1.close();
     }
+     public void StudentDetail() throws SQLException {
+        /******************/
+        /* JDBC Statement */
+        /******************/
+
+        System.out.println("\n");
+        System.out.println("--------------");
+        System.out.println("JDBC Statement");
+        System.out.println("--------------");
+
+        String query1 = "SELECT distinct sc.first, sc.last, ca.RID, p.Payment, cr.TID, t.resort, t.sun_date, t.city, t.state, cr.unit_no, cr.bldg "
+        		+ "FROM SkiClub sc "
+        		+ "inner join Condo_Assign ca "
+        		+ "on sc.MID = ca.MID "
+        		+ "inner join Payment p on "
+        		+ "ca.mid = p.mid "
+        		+ "inner join Condo_Reservation cr on "
+        		+ "p.RID = cr.RID "
+        		+ "inner join Trip t on "
+        		+ "cr.tid = t.tid "
+        		+ "Where first = 'Matt'";
+
+        System.out.println("\nStatement: " + query1 + "\n");
+
+        Statement stmt1 = newCon.createStatement ();
+        ResultSet rset1 = stmt1.executeQuery(query1);
+        while (rset1.next ()) {
+            System.out.println("Student Name: " + rset1.getString("first") + " " + rset1.getString("last") 
+            		+ "\nResort Information: " + rset1.getString("resort") + " " + rset1.getDate("sun_date") + " "
+            		+ rset1.getString("city") + ", " + rset1.getString("state") + " " 
+            		+ "\nRoom Information: " + rset1.getString("RID") + " " + rset1.getString("unit_no")+ " " + rset1.getString("bldg") + " " 
+            		+ rset1.getString("tid") + " " + "\nPayment: " +  rset1.getString("payment")+ "\n\n");
+        }
+        // Release the statement and result set
+        stmt1.close();
+        rset1.close();
+    }
 
 }
