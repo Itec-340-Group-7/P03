e9deb5735d8d480db9184fc22e03ea88d2875c10
shorton6 <38045199+shorton6@users.noreply.github.com>
2018-04-26 19:23:56 -0400

    1

--- DataSource.java
+++ DataSource.java
@@ -3,10 +3,12 @@ package connection;
 import java.sql.*;
 
 /**
- * Steven Horton
- * Austin Crockett
- * Colin Ryan
- *
+ * @author Steven Horton - Completed the tripDetail query ensuring it returned the proper values and coded/formatted queries for the reports.java
+ * @author Austin Crockett - coded the ReportDrv.java and the condoDetail, financialDetail queries ensuring they returned the proper values
+ * @author Colin Ryan - coded the DataSource.java and studentDetail query ensuring it returned the proper values
+ * @date 4/26/2018
+ * ITEC 340 - P03
+ * DataSource.java
  */
 public class DataSource {
     private String user = "";
@@ -15,8 +17,8 @@ public class DataSource {
 
     public DataSource() throws SQLException {
 
-            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@Picard2:1521:itec2", user, pass);
+        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        conn = DriverManager.getConnection("jdbc:oracle:thin:@Picard2:1521:itec2", user, pass);
     }
 
     public Connection getConn(){
--- ReportDrv.java
+++ ReportDrv.java
@@ -2,12 +2,23 @@ package connection;
 
 import java.sql.SQLException;
 /**
- * Steven Horton
- * Austin Crockett
- * Colin Ryan
+ * @author Steven Horton - Completed the tripDetail query ensuring it returned the proper values and coded/formatted queries for the reports.java
+ * @author Austin Crockett - coded the ReportDrv.java and the condoDetail, financialDetail queries ensuring they returned the proper values
+ * @author Colin Ryan - coded the DataSource.java and studentDetail query ensuring it returned the proper values
+ * @date 4/26/2018
+ * ITEC 340 - P03
+ * ReportDrv.java
  */
 public class ReportDrv {
-    private static Reports report = new Reports();
+    private static Reports report;
+
+    static {
+        try {
+            report = new Reports();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String [] args) throws SQLException {
         report.run();
